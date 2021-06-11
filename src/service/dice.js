9364f6cc1a1f7da6858db729596a2821687e49fb
Lais Pereira <lais.pereira.santos92@gmail.com>
2021-06-11 20:49:21 -0300

    adicionando arquivo json

---
+++ src/service/dice.js
@@ -0,0 +1,43 @@
+const shrek=[
+  {
+    id:1,
+    nome:"Shrek",
+    descrição:"Shrek",
+    img: "https://upload.wikimedia.org/wikipedia/pt/e/ed/Shrek%28personagem%29.jpg"
+  },
+  
+  {
+    id:2,
+    nome:"Fiona",
+    descrição:"Fiona",
+    img: "https://i.pinimg.com/originals/17/11/42/171142be15ad76710183193d68084a3b.jpg"
+  
+  },
+  {
+    id:3,
+    nome:"Burro",
+    descrição:"Burro",
+    img: "https://i.pinimg.com/originals/6b/68/f8/6b68f84c63c4bcd8d4e600ed593650cb.jpg"
+  
+  },
+  {
+    id:4,
+    nome:"Homem Biscoito",
+    descrição:"Homem Biscoito",
+    img: "https://i.pinimg.com/originals/91/f1/d2/91f1d28c883a0295c813943101be6e81.png"
+  
+  },
+  {
+    id:5,
+    nome:"Lord Farquaad",
+    descrição:"Lord Farquaad",
+    img: "https://upload.wikimedia.org/wikipedia/en/9/91/LordFarquaad%28ShrekCharacter%29.png"
+  
+  }
+  
+  
+  ]
+  
+  
+  
+  export default shrek;
